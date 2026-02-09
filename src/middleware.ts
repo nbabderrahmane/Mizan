4d3577dd406ab9f12f0a79ef92efe55a9b7243d8
Abderrahmane Naciri Bennani <abderrahmanenaciribennani@gmail.com>
2026-02-09 13:58:35 +0100

    resolution google auth

--- src/middleware.ts
+++ src/middleware.ts
@@ -23,6 +23,6 @@ export const config = {
         // - _next/image (image optimization files)
         // - favicon.ico (favicon file)
         // - public folder
-        "/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)",
+        "/((?!api|_next/static|_next/image|favicon.ico|auth/callback|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)",
     ],
 };
